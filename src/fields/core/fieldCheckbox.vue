13c2a9483a6caa747b12ef442c705cbf8a268076
paulogiacomelli <paulogiacomelli@hotmail.com>
2020-05-06 12:56:29 -0700

    testing checkbox being a switch

--- src/fields/core/fieldCheckbox.vue
+++ src/fields/core/fieldCheckbox.vue
@@ -1,5 +1,16 @@
-<template lang="pug">
-	input(:id="getFieldID(schema)", @blur="onDataAccept", type="checkbox", v-model="value", :autocomplete="schema.autocomplete", :disabled="disabled", :name="schema.inputName", :class="schema.fieldClasses", :required="schema.required", v-attributes="'input'")
+<template>
+	<div :class="schema.fieldClasses" class="custom-control custom-switch">
+		<input :id="getFieldID(schema)"
+		type="checkbox"
+		v-model="value"
+		:autocomplete="schema.autocomplete"
+		:disabled="disabled"
+		:name="schema.inputName"
+		:required="schema.required"
+		v-attributes="'input'" >
+      <label class="custom-control-label" :for="getFieldID(schema)">Toggle this switch element</label>
+	</div>
+
 </template>
 
 <script>
