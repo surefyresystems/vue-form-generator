c78f4c9f90386cf712d4425f7b840bb854290490
Dustin Torres <dustingtorres@gmail.com>
2020-11-12 16:20:25 -0800

    Remove PUG from formGenerator.vue

--- src/formGenerator.vue
+++ src/formGenerator.vue
@@ -1,14 +1,26 @@
-<template lang="pug">
-div.vue-form-generator(v-if='schema != null')
-	fieldset(v-if="schema.fields", :is='tag')
-		template(v-for='field in fields')
-			form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options", @validated="onFieldValidated", @model-updated="onModelUpdated")
+<template>
+	<div class="vue-form-generator" v-if="schema != null">
+		<fieldset v-if="schema.fields" :is="tag">
+			<template v-for="(field, index) in fields">
+				<form-group v-if="fieldVisible(field)" :key="index" :vfg="vfg" :field="field" :errors="errors" :model="model" :options="options" @validated="onFieldValidated" @model-updated="onModelUpdated">
+				</form-group>
+			</template>
+		</fieldset>
 
-	template(v-for='group in groups')
-		fieldset(:is='tag', :class='getFieldRowClasses(group)')
-			legend(v-if='group.legend') {{ group.legend }}
-			template(v-for='field in group.fields')
-				form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options", @validated="onFieldValidated", @model-updated="onModelUpdated")
+		<template v-for="(group, index) in groups">
+			<fieldset :is="tag" :class="getFieldRowClasses(group)" :key="index">
+				<legend v-if="group.legend">
+					{{group.legend}}
+				</legend>
+				<template v-for="(field, index) in group.fields">
+					<form-group v-if="fieldVisible(field)" :key="index" :vfg="vfg" :field="field" :errors="errors" :model="model" :options="options" @validated="onFieldValidated" @model-updated="onModelUpdated">
+					</form-group>
+				</template>
+			</fieldset>
+
+		</template>
+
+	</div>
 </template>
 
 <script>
